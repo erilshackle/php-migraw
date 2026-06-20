59b4e4bdb7cf50ed06681715356ef1ff12280d8e
Eril TS Carvalho <erilandocarvalho@gmail.com>
2026-06-20 04:00:54 -0100

    feat: add table manipulation methods to Migration class

--- src/Migration.php
+++ src/Migration.php
@@ -2,6 +2,7 @@
 
 namespace Eril\Migraw;
 
+use Eril\Migraw\Sql\Sql;
 use Eril\Migraw\Sql\SqlStatement;
 
 abstract class Migration
@@ -15,4 +16,24 @@ abstract class Migration
      * SQL executado ao reverter a migration.
      */
     abstract public function down(): string|array|SqlStatement;
+
+    protected function createTable(String $table, bool $ifNotExists = false)
+    {
+        return Sql::create($table, $ifNotExists);
+    }
+
+    protected function alterTable(String $table)
+    {
+        return Sql::alter($table);
+    }
+
+    protected function dropTable(String $table, bool $ifExists = false)
+    {
+        return Sql::drop($table, $ifExists);
+    }
+
+    protected function renameTable(String $table)
+    {
+        return Sql::rename($table);
+    }
 }
